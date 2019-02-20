15ead6b34ed94231dc347bced08e8d0a13a3a3f3
github username <github email address>
2019-02-20 17:59:36 +0000

    for Khang

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,85 @@
 # your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def name #getter attr_reader
+    @name
+  end
+
+  def name=(new_name) #setter attr_writer
+    @name = new_name
+  end
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(happiness)
+    if happiness > 10
+      @happiness = 10
+    elsif happiness < 0
+      @happiness = 0
+    else
+      @happiness = happiness
+    end
+  end
+
+  def hygiene=(hygiene)
+    if hygiene > 10
+      @hygiene = 10
+    elsif hygiene < 0
+      @hygiene = 0
+    else
+      @hygiene = hygiene
+    end
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene=(self.hygiene+4)
+    self.hygiene += 4 #self.hygiene = self.hygiene + 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    '♪ another one bites the dust ♫'
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    if friend.name == "Felix"
+      "Hi #{friend}! It's #{@name}. How are you?"
+    end
+  end
+
+end
+
+
+#
+#
+# p1 = Person.new("Khang")
+#
+#
+# binding.pry
+# "lalal"
