85a0dd8f5e864fdef8eb1a0043e5a7126a044082
schazbot <charlotte.pearce1984@gmail.com>
2019-08-07 11:35:28 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,70 +1,82 @@
-# your code goes here
+require "pry"
+
 class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
 
-attr_accessor :bank_account, :happiness, :hygiene
-attr_reader :name
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
 
-def initialize(name)
-  @name = name
-  @bank_account = 25
-  @happiness = 8
-  @hygiene = 8
-end
-
-def happiness=(number)
-  @happiness = number
-  @happiness = 10 if @happiness > 10
-  @happiness = 0 if @happiness < 0
-end
-
-def hygiene=(number)
-  @hygiene = number
-  @hygiene = 10 if @hygiene > 10
-  @hygiene = 0 if @hygiene < 0
-end
-
-def happy?
-  @happiness > 7
-end
-
-def clean?
-  @hygiene > 7
-end
-
-def get_paid(salary)
-  self.bank_account += salary
- "all about the benjamins"
-end
-
-def take_bath
-  self.hygiene += 4
-  "♪ Rub-a-dub just relaxing in the tub ♫"
-end
-
-def work_out
-  self.hygiene -= 3
-  self.happiness += 2
-  "♪ another one bites the dust ♫"
-end
-
-def call_friend(friend)
-  friend.happiness += 3
-  self.happiness += 3
-  "Hi #{friend.name}! It's #{self.name}. How are you?"
-end
-
-def start_conversation(friend, topic)
-    if topic == "politics"
-      friend.happiness -= 2
-      self.happiness -= 2
-      "blah blah partisan blah lobbyist"
-    elsif
-      topic == "weather"
-      friend.happiness += 1
-      self.happiness += 1
-      "blah blah sun blah rain"
-    else
-      "blah blah blah blah blah"
+  def happiness=(number)
+    @happiness = number
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
     end
-end
+  end
+
+  def hygiene=(number)
+    @hygiene = number
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    end
+  end
+
+  def happy?
+    if self.happiness > 7
+      return true
+    else false     end
+  end
+
+  def clean? 
+    if self.hygiene > 7
+      return true
+    else false     end
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath 
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      self.happiness -= 2 
+      friend.happiness -= 2
+      return "blah blah partisan blah lobbyist"
+
+    elsif topic == "weather"
+      self.happiness += 1 
+      friend.happiness += 1
+      return "blah blah sun blah rain"
+
+    elsif topic != "politics" || topic != "politics"
+      return "blah blah blah blah blah"
+    end
+  end
+
 end
